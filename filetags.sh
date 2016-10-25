2f3856631e6eb61df9341129108a1b0a93e5a55a
yanqing4 <yanqing4@staff.sina.com.cn>
2016-10-25 11:22:09 +0800

    modify fileags.sh

--- filetags.sh
+++ filetags.sh
@@ -1,4 +1,4 @@
-# generate tag file for lookupfile plugin
+#!/bin/sh
 
 basepath="/data0/sae"
 
@@ -10,24 +10,8 @@ do
     tagfile=".filenametags"
     ctagfile=".tags"
 
-    # if [ ! -s $tagfile ]
-    # then
-        echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > $tagfile
-        find . -not -iregex '.*\.\(git\|jpg\|png\|gif\)' -type f -printf "%f\t%p\t1\n" |  sort -f >> $tagfile
-    # fi
-
-    # if [ ! -s $ctagfile ]
-    # then
-        ctags \
-            -f $ctagfile\
-            -R -h ".php" \ 
-            --exclude="\.git|\.swp|\.swo|\.png|\.jpg|\.gif|" \
-            --regex-php="/^[ \t]*[(private|public|static)( \t)]*function[ \t]+([A-Za-z0-9_]+)[ \t]*\(/\1/f, function, functions/" \
-            --regex-php="/^[ \t]*[(private| public|static)]+[ \t]+\$([A-Za-z0-9_]+)[ \t]*/\1/p, property, properties/" \
-            --regex-php="/^[ \t]*(const)[ \t]+([A-Za-z0-9_]+)[ \t]*/\2/d, const, constants/" \
-            --languages=php 
-    # fi
-
+    echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > $tagfile
+    find . -not -iregex '.*\.\(git\|jpg\|png\|gif\)' -type f -printf "%f\t%p\t1\n" |  sort -f >> $tagfile
+    ctags -f $ctagsfile -R --c++-kinds=+p --fields=+iaS --extra=+qf 2>/dev/null
     cd -
 done
-
