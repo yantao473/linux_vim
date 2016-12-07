87e7e86404eb7e2203b058872d45957d05c03a1e
yanqing4 <yanqing4@staff.sina.com.cn>
2016-12-07 15:27:22 +0800

    change filetags.sh for js

--- filetags.sh
+++ filetags.sh
@@ -1,17 +1,46 @@
 #!/bin/sh
 
-basepath="/data0/sae"
 
+basepath="/data0/sae"
 cd $basepath
 
-ls  | while read path
-do
-    cd $path
-    tagfile=".filenametags"
-    ctagfile=".tags"
+gentags()
+{
+    tf="$1/.filenametags"
+    ctf="$1/.tags"
 
-    echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > $tagfile
-    find . -not -iregex '.*\.\(git\|jpg\|png\|gif\)' -type f -printf "%f\t%p\t1\n" |  sort -f >> $tagfile
-    ctags -f $ctagsfile -R --c++-kinds=+p --fields=+iaS --extra=+qf 2>/dev/null
-    cd -
-done
+    echo $1
+    cd $1
+
+    echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > $tf
+
+    find -not -iregex '.*\.\(jpg\|png\|gif\)' -type f -printf "%f\t%p\t1\n" | grep -v '.git'  |  sort -f >> $tf
+
+   ctags \
+       -f $ctf \
+       -R \
+       --exclude=.git \
+       --exclude=*.min.js \
+       --exclude=bower_components/* \
+       --exclude=node_modules/* \
+       --exclude=*.css \
+       --exclude=*.jpg \
+       --exclude=*.gif \
+       --exclude=*.bmp \
+       --exclude=test \
+       --fields=+iaS\
+       --extra=+qf \
+       2>/dev/null
+
+}
+
+
+if [ "x${1}x" != "xx" ]
+then
+    gentags "$basepath/$1"
+else
+    ls  | while read propath
+    do
+        gentags "$basepath/$propath"
+    done
+fi
